cbc43f14296c8125f106d55e8097a64c34adfb25
LouisVantaoo-oss <louisfernando2006@gmail.com>
2026-08-31 19:05:50 +0700

    versi 1

---
+++ tipedata.dart
@@ -0,0 +1,10 @@
+void main() {
+  String nama = 'Deni';
+  int umur = 30;
+  bool sudahmenikah = false;
+
+  var nama1 = 'Deni';
+  var angka1 = 100;
+
+  print ('halo nama saya $nama. Umur saya $umur. sudah menikah $sudahmenikah');
+}

89fbff513046527f3831fc514e2b6047ba1767eb
LouisVantaoo-oss <louisfernando2006@gmail.com>
2026-08-31 20:30:40 +0700

    versi 1

--- tipedata.dart
+++ tipedata.dart
@@ -1,10 +1,71 @@
+
 void main() {
-  String nama = 'Deni';
-  int umur = 30;
-  bool sudahmenikah = false;
+  //String nama = 'Deni';
+  //int umur = 30;
+  //bool sudahmenikah = false;
 
-  var nama1 = 'Deni';
-  var angka1 = 100;
+  //var nama1 = 'Deni';
+  //var angka1 = 100;
 
-  print ('halo nama saya $nama. Umur saya $umur. sudah menikah $sudahmenikah');
+  //print ('halo nama saya $nama. Umur saya $umur. sudah menikah $sudahmenikah');
+
+  //int angka1 = 100;
+  //double angka2 = 20,5;
+  
+  // num hasil = 0;
+  //hasil = angka1 + angka2;
+  // print(hasil);
+
+ //dynamic angka1 = 100;
+ //dynamic angka2 = 200,5;
+ //dynamic hasil = 0;
+ //hasil = angka1 + angka2
+ //rint (hasil);
+
+ //var angka1 = 100;
+ //var angka2 = 200.5;
+ //var hasil = 0.5;
+ //hasil = angka1 + angka2;
+ //print(hasil);
+
+//list (array) => kumpulan data yang memiliki index
+ //List<dynamic> namamhs= ['Deni','Sindy','miki'];
+ //List<String> nama= ['Deni','Sindy','miki', 100];
+ //List<num> angka= [12, 12.5, 13.0, -1, 12.5];
+ //print(namamhs);
+
+//  List namamhs = [];
+//  namamhs.add('Wilson1');
+//  namamhs.add('Wilson2');
+//  namamhs.add('Wilson3');
+//  namamhs.add('Wilson4');
+//  print(namamhs.first);
+
+// Set => array yang unik
+// Set angka = {1, 2, 3, 4, 1, 2, 3, 4};
+// print(angka.elementAt(2));
+
+// map  => array yang memiliki key dan value
+
+// Map datamhs = {'nama' : 'ferry', 'umur' : 30, 'kelas' : 'A'};
+// print (datamhs['kelas']);
+
+// List<Map> datamhs= [
+//   {'nama' : 'Anton', 'umur': 30, 'kelas' : 'A'};
+//   {'nama' : 'Deka', 'umur': 20, 'kelas' : 'P'};
+//   {'nama' : 'Ferry', 'umur': 25, 'kelas' : 'Q'};
+// ]
+//  print('halo nama saya $(datamhs[1]['kelas']')
+
+//  final string nama;
+//  nana = 'Ferry';
+//  //nama = 'deka';
+
+//  const int angka = 100;
+//  angka = 200;
+
+// dart null safety
+
+// String? nama;
+// print(nama);
 }
